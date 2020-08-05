27e690215ca943b95287304e24544d07325d8a62
zkynet <sveinn@zkynet.io>
2020-08-05 20:01:49 +0000

    LESSON 1 ENDS HERE

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-
+	"github.com/zkynet/golang-lessons-for-beginners/src/files"
 	"github.com/zkynet/prettyprint"
 )
 
 func main() {
-	currentDirectory := CreateFileMap(".")
+	currentDirectory := files.CreateFileMap(".")
 	prettyprint.PrintFileMap(currentDirectory)
-}
-
-// CreateFileMap ...
-// This function parses the directory given as input and
-// returns a map of files, the index being the full
-// file path and the value being the file ext.
-func CreateFileMap(dir string) map[string]os.FileInfo {
-	fileMap := make(map[string]os.FileInfo)
-
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		fileMap[dir+"/"+path] = info
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	return fileMap
 }
