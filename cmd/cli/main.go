1339e1857ee1dcacc81f1036efdb82a973995953
zkynet <anon@zkynet.io>
2020-08-10 19:28:52 +0000

    implementing basic search and cleaning up a bit

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/zkynet/golang-lessons-for-beginners/src/files"
-	"github.com/zkynet/prettyprint"
 )
 
 func main() {
@@ -13,7 +12,8 @@ func main() {
 		log.Println("You need to specify the directory you want to start from..")
 		os.Exit(1)
 	}
-	currentDirectory := files.CreateFileMap(os.Args[1])
-	sortedSizes := files.SortBySize(currentDirectory)
-	prettyprint.PrintFileMapBySize(currentDirectory, sortedSizes)
+	fileMap := files.CreateFileMap(os.Args[1])
+	// sortedSizes := files.SortBySize(currentDirectory)
+	// prettyprint.PrintFileMap(currentDirectory)
+	files.WordSearch(fileMap, "meow")
 }
