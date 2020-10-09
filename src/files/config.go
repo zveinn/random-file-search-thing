2f4aa5628574b7f6362323888e0eca796a398241
zkynet <anon@zkynet.io>
2020-10-09 18:26:08 +0000

    removing the need for a config

--- src/files/config.go
+++ src/files/config.go
@@ -1,32 +1,26 @@
 package files
 
-import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/user"
-)
-
 var GlobalConfig *Config
 
 func LoadConfig() {
 
-	user, err := user.Current()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+	// user, err := user.Current()
+	// if err != nil {
+	// 	log.Fatalf(err.Error())
+	// }
 
-	data, err := ioutil.ReadFile(user.HomeDir + "/search.json")
-	if err != nil {
-		log.Println("Could not find config file ...")
-		os.Exit(1)
-	}
+	// data, err := ioutil.ReadFile(user.HomeDir + "/search.json")
+	// if err != nil {
+	// 	log.Println("Could not find config file ...")
+	// 	os.Exit(1)
+	// }
 	GlobalConfig = new(Config)
-	err = json.Unmarshal(data, GlobalConfig)
-	if err != nil {
-		log.Println("Could not read/parse the config file ...")
-		os.Exit(1)
-	}
+	// err = json.Unmarshal(data, GlobalConfig)
+	// if err != nil {
+	// 	log.Println("Could not read/parse the config file ...")
+	// 	os.Exit(1)
+	// }
+	GlobalConfig.Ignore = []string{".gitignire", ".exe", ".git"}
+	GlobalConfig.MaxFileSize = 2
 
 }
